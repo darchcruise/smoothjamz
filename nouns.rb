c9873cecffc811dffe48a34927c0dbda97c7a9e9
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2013-06-22 13:32:00 -0400

    done with that one

--- nouns.rb
+++ nouns.rb
@@ -1,1 +1,18 @@
 puts "Hey girl. What's your name?"
+name = gets.chomp
+# string interpolation uses double quotes
+
+MoralCompass = 18
+
+puts "That's a beautiful name, #{name}"
+puts "hey girl. how old are you?"
+age = gets.chomp.to_i
+if age > MoralCompass
+  puts "Sweet. Let me get your number."
+else
+  puts "is this to catch a predator?"
+  # raise causes an error
+  raise "Too Young"
+end
+
+
